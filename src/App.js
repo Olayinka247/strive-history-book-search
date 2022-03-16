0bfeeb09039d6ff43ffac6f5dcbc36a44685b294
Olayinka247 <97066655+Olayinka247@users.noreply.github.com>
2022-03-16 19:17:33 +0000

    badge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import WarningSign from "./component/WarningSign";
+import MyBadge from "./component/MyBadge";
 
 function App() {
   return (
     <div className="App">
       <WarningSign text="Latest History Books" />
+      <MyBadge color="success" text="OUT NOW !" />
     </div>
   );
 }
