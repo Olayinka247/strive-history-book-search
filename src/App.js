cb39ecfdbd5118242d8779a193f818c101a32d4e
Olayinka247 <97066655+Olayinka247@users.noreply.github.com>
2022-03-24 14:32:33 +0000

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WarningSign from "./component/WarningSign";
 import MyBadge from "./component/MyBadge";
 import SingleBook from "./component/SingleBook";
@@ -8,14 +9,18 @@ import BookList from "./component/BookList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <WarningSign text="Latest History Books" />
-        <MyBadge color="success" text="OUT NOW !" />
-        <SingleBook book={history[0]} />
-        <BookList books={history} />
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <header className="App-header">
+            <WarningSign text="Latest History Books" />
+            <MyBadge color="success" text="OUT NOW !" />
+            <SingleBook book={history[0]} />
+            <BookList books={history} />
+          </header>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
